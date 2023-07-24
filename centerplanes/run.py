9d139226c1355dd4f441efe9dcd9b2995d208ff3
1000zoo <cjswldn99@gmail.com>
2023-07-24 14:11:37 +0900

    미리 지정한 점들로 중심선계산

--- centerplanes/run.py
+++ centerplanes/run.py
@@ -3,10 +3,11 @@ from center import *
 import json
 
 
-PATH = lambda x : f"/mnt/nvme1n1/JW/crps/rois_tracts_bin/tract_{x}.nii.gz"
+# PATH = lambda x : f"/mnt/nvme1n1/JW/crps/rois_tracts_bin/tract_{x}.nii.gz"
+PATH = lambda x : f"/Users/1000zoo/Documents/crps_data/crps/rois_tracts_bin/tract_{x}.nii.gz"
 
 if __name__ == "__main__":
-    for i in range(1, 21):
+    for i in range(15,16):
         tract = nib.load(PATH(i)).get_fdata()
         t = Tract(tract, i)
         t.plot()
